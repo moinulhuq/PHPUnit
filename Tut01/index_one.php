33fd16a73c27376a92e33c4093fc19cb8bec7c18
Md Moinul Huq <moinulhuq@users.noreply.github.com>
2015-09-08 17:14:30 +1200

    Create index_one.php

---
+++ Tut01/index_one.php
@@ -0,0 +1,1 @@
+First, we need to define our test suite configuration in the phpunit.xml file and place it in my PHPUnit folder. In this case, we specify that our “SimpleProject” folder will contain al
