1c4a9a1b993e84fc853f08934b4228795601d7ef
Md Moinul Huq <moinulhuq@users.noreply.github.com>
2015-09-14 13:30:10 +1200

    Create Simple1.php

---
+++ Tut03/Simple1.php
@@ -0,0 +1,13 @@
+<?php
+/*
+|--------------------------------------------------------------------------
+| Another Example Of PHPUnit test procedural PHP program
+|--------------------------------------------------------------------------
+*/
+
+$arg1 = $_GET['arg1'];
+$arg2 = $_GET['arg2'];
+$return = (int)$arg1 + (int)$arg2;
+return (int)$return;
+
+?>
