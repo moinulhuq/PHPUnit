06b2fd7ad2956f3df9fd44b160f83bebbaeed40a
Md Moinul Huq <moinulhuq@users.noreply.github.com>
2015-09-14 12:41:39 +1200

    Update Simple1Test.php

--- Tut02/Test/Simple1Test.php
+++ Tut02/Test/Simple1Test.php
@@ -4,7 +4,7 @@ class Simple1Test extends PHPUnit_Framework_TestCase
 {
 
     public function testadd() {    	
-        include_once(realpath(__DIR__.'/../Simple1.php'));
+        include_once(realpath(__DIR__.'/../Simple1.php')); //Must include as non OOP function
         $result =  add(1,1); 
         $this->assertEquals(2, $result);
     }
