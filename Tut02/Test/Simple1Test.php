025aa8ef923df05349891360a0949edc210ecd1d
Md Moinul Huq <moinulhuq@users.noreply.github.com>
2015-09-14 12:32:30 +1200

    Update Simple1Test.php

--- Tut02/Test/Simple1Test.php
+++ Tut02/Test/Simple1Test.php
@@ -1,10 +1,10 @@
 <?php
 
-class Simple1Test extends PHPUnit_Framework_TestCase
+class Simple1Test extends \PHPUnit_Framework_TestCase
 {
 
-    public function testadd() {
-        include("Simple1.php");
+    public function testadd() {    	
+        include_once(realpath(__DIR__.'/../Simple1.php'));
         $result =  add(1,1); 
         $this->assertEquals(2, $result);
     }
