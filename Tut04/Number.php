32eca41648788b77b17045660ca8ab9f79d512ad
Md Moinul Huq <moinulhuq@users.noreply.github.com>
2015-09-15 13:27:12 +1200

    Update Number.php

--- Tut04/Number.php
+++ Tut04/Number.php
@@ -8,13 +8,23 @@
 class Number{
 
 	public $Number;
-
+	public $Arr;
+	
 	public function __construct($Number){
 			$this->Number = $Number;
+			$this->setArr();
 	}
 
 	public function getNumber(){
 			return $this->Number;
+	}
+
+	public function setArr(){
+			$this->Arr = array('key1' => 'Value 1', 'key2' => 'Value 2');
+	}
+
+	public function getArr(){
+			return $this->Arr;
 	}
 
 }
