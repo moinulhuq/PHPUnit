f8fc4d2506a1a15ce5f198e25f085263eca14403
Md Moinul Huq <moinulhuq@users.noreply.github.com>
2015-09-29 07:41:34 +1300

    Update DummyObjTest.php

--- Tut08/Test/DummyObjTest.php
+++ Tut08/Test/DummyObjTest.php
@@ -1,18 +1,18 @@
 <?php
 /*
 |--------------------------------------------------------------------------
-| Dummy Objects - Test Class Example
+| Dummy - Skeleton
 |--------------------------------------------------------------------------
-| Here we want to test "processFoo()" and "mergeBar()" methods of "Baz" class.
-| 
-| To do this we need to create Dummy Objects of "Foo" and "Bar" class and pass it to "Baz" Class. Here we are going to 
-| immitate/fake "Foo" and "Bar", these two object function so that we can run test.
 |
-| Here we want to test "processFoo()" and "mergeBar()" methods of "Baz" class.
+| $foo = $this->getMock('Foo', array('process')); 
+|   (or)
+| $foo = $this->getMockBuilder('Foo')
+|             ->setMethods(array('process'))
+|              ->getMock(); 
 |
 */
 
-require_once(realpath(__DIR__.'/../Baz.php'));
+require_once(realpath(__DIR__.'/../Dummy.php'));
 
 class DummyObjTest extends PHPUnit_Framework_TestCase
 { 
@@ -24,23 +24,21 @@ class DummyObjTest extends PHPUnit_Framework_TestCase
     }
     public function testprocessFoo(){
         
-        //Creating "Foo" Dummy object
-        //$foo = $this->getMock('Foo', array('process')); // (or)
+        //$foo = $this->getMock('Foo', array('process')); 
+        // (or)
         $foo = $this->getMockBuilder('Foo')
                     ->setMethods(array('process'))
                     ->getMock(); 
 
-        //Configuring "Foo" Dummy object
         $foo->expects($this->once())
             ->method('process')
             ->will($this->returnValue(1));
 
-        //Creating "Bar" Dummy object but no need to define methods "getStatus()" and "merge()" of this object because
-        //At "processFoo()" of Baz Class there is no use of these methods
-        //$bar = $this->getMock('Bar'); // (or)
+        /*Our dummy object in this test is the bar object*/
+        //$bar = $this->getMock('Bar'); 
+        // (or)
         $bar = $this->getMockBuilder('Bar')
                     ->getMock(); 
-        /*Our dummy object in this test is the bar object*/
         
         $baz = new Baz($foo, $bar);
 
@@ -48,9 +46,9 @@ class DummyObjTest extends PHPUnit_Framework_TestCase
     }
 
     public function testmergeBar(){
+        /*Our dummy object in this test is the "Foo" object*/
         $foo = $this->getMockBuilder('Foo')
                     ->getMock(); 
-        /*Our dummy object in this test is the Foo object*/            
         
         $bar = $this->getMockBuilder('Bar')
                     ->setMethods(array('getStatus', 'merge'))        
