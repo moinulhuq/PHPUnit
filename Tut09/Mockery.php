e21f580ab7f716012fd6d4fe9c5e4d4c5fafb056
Md Moinul Huq <moinulhuq@users.noreply.github.com>
2015-09-30 08:47:15 +1300

    Create Mockery.php

---
+++ Tut09/Mockery.php
@@ -0,0 +1,23 @@
+<?php
+/*
+|--------------------------------------------------------------------------
+| Mockery
+|--------------------------------------------------------------------------
+| -> Mockery is a powerful Mock Object framework for PHP testing. Although PHPUnit does actually ship with a Mock Object library, 
+| I think most people prefer to use Mockery instead.
+| 
+| -> For bigger projects and complex code, Mockery might offer a few tricks which really help.
+|
+*/
+
+class AClassToBeMocked {
+
+}
+
+class AClassToWorkWith {
+    function doSomethingWit($anotherClass) {
+        return $anotherClass->someMethod();
+    }
+}
+
+?>
